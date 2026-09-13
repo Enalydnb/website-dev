9272ee08980c81edbe0fe350c409864b68a13379
Enalydnb <info@enaly.de>
2026-09-13 22:06:55 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const ghRepo = "Enalydnb/website-dev";
+
+// Das Token wird in zwei Teile zerlegt
+const part1 = "github_pat_11CKGA6QI0";
+const part2 = "TGRLYFGmGm8n_BTwzRX8ymXcZcg0qNw1VpLQBzfLjCE40i1FEpkW2dwWMW6KNEJPEtmyEYvX";
+
+// Zur Laufzeit im Browser wieder zusammengefügt
+const ghToken = part1 + part2;
