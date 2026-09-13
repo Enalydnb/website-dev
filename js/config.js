1b20ccf2fbc62ea0c38abecef60b13db78af97ee
Enalydnb <info@enaly.de>
2026-09-13 22:16:58 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,8 +1,4 @@
-const ghRepo = "Enalydnb/website-dev";
+// js/config.js
+const proxyApiUrl = "https://website-api-dev.quiet-rain-dcf1.workers.dev";
 
-// Das Token wird in zwei Teile zerlegt
-const part1 = "github_pat_11CKGA6QI0";
-const part2 = "TGRLYFGmGm8n_BTwzRX8ymXcZcg0qNw1VpLQBzfLjCE40i1FEpkW2dwWMW6KNEJPEtmyEYvX";
 
-// Zur Laufzeit im Browser wieder zusammengefügt
-const ghToken = part1 + part2;
